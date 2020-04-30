cc50d429c05b52e2d59a82b226725167b727ae43
H4RP3R <zombiehunter-vm@yandex.ru>
2020-04-30 12:09:36 +0300

    button added, minor fixes

--- checkbox.js
+++ checkbox.js
@@ -1,3 +1,5 @@
+let cookieList = []
+
 function getCookie(name) {
     let matches = document.cookie.match(new RegExp(
         "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
@@ -27,7 +29,28 @@ $(document).ready(function() {
     $("input[type='checkbox']").each(function() {
         $(this).on('click', function() {
             // create or update cookie status on every click
-            document.cookie = `${$(this).attr('id')}=${$(this).prop('checked')}; max-age=3600`;
+            document.cookie = `${$(this).attr('id')}=${$(this).prop('checked')}; max-age=36000`;
         });
     });
+
+    $('#save').click(function() {
+        // check if at least one is checked
+        let checked = []
+        $("input[type='checkbox']").each(function() {
+            if ($(this).prop("checked") === true) {
+                checked.push(this)
+            }
+        });
+        if (checked.length > 0) {
+            // save cookies from the list
+            cookieList.forEach(element => document.cookie = element);
+            // disable checkboxes after saving
+            $("input[type='checkbox']").each(function() {
+                $(this).attr("disabled", true);
+            });
+            $('.alert-success').fadeIn(600).delay(2000).fadeOut(1000);
+        } else {
+            $('.alert-primary').fadeIn(600).delay(2000).fadeOut(1000);
+        }
+    })
 });
